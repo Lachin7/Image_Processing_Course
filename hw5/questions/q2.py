fcc6c494366fa517e367d2d8a15688d669256903
Lachin7 <lachin.naghashyar@gmail.com>
2022-02-05 21:35:05 +0330

    made the .ipynb files

--- hw5/questions/q2.py
+++ hw5/questions/q2.py
@@ -1,60 +1,50 @@
 import cv2
+import matplotlib.pyplot as plt
 import numpy as np
 import scipy.sparse as sp
 from scipy.sparse.linalg import spsolve
 
-img_src = cv2.imread("../images/res05.jpg")
-img_tar = cv2.imread("../images/res06.jpg")
-mask = cv2.imread("../images/dog_mask.jpg", 0)
 
-n, m, _ = img_tar.shape
-translation_mat = np.float32([[1, 0, 290], [0, 1, 402]])
-img_src = cv2.warpAffine(img_src, translation_mat, (m, n))
-mask = cv2.warpAffine(mask, translation_mat, (m, n))
-mask[mask < 0.1] = 0
+def apply_poisson_blending(src_name, tar_name, mask_name, x_off, y_off):
+    img_src = cv2.imread("../images/" + src_name + ".jpg")
+    img_tar = cv2.imread("../images/" + tar_name + ".jpg")
+    mask = cv2.imread("../images/" + mask_name + ".jpg", 0)
+    # they don't have the same shapes so I used an affine transformation to do so
+    n, m, _ = img_tar.shape
+    translation_mat = np.float32([[1, 0, x_off], [0, 1, y_off]])
+    img_src = cv2.warpAffine(img_src, translation_mat, (m, n))
+    mask = cv2.warpAffine(mask, translation_mat, (m, n))
+    # some of the pixels might be other than 0 and 255 so if it was the case, make them be 0 or 255
+    mask[mask < 0.1] = 0
+    return solve_poisson_equ(img_src, img_tar, mask, n, m)
 
 
-def laplacian_mat():  # generate the laplacian matrix
+def laplacian_mat(n, m):  # generate the laplacian matrix
     data, I = np.ones((m * m)), np.eye(m)
     sparse_matrix = sp.spdiags(np.array([data, -2 * data, data]), np.array([-1, 0, 1]), n, n)
     return -1 * (sp.kron(I, sparse_matrix) + sp.kron(sparse_matrix, I)).tolil()
 
 
-A = laplacian_mat()
-lap = A.tocsc()
+def solve_poisson_equ(img_src, img_tar, mask, n, m):
+    A = laplacian_mat(n, m)
+    lap = A.tocsc()
 
-# since the area does not necessarily have a rectangular shape, we should set the ones outside this region to identity.
-for y in range(1, n - 1):
-    for x in range(1, m - 1):
-        if mask[y, x] == 0:
-            z = x + y * m
-            A[z, z], A[z, z + m], A[z, z - m], A[z, z + 1], A[z, z - 1] = 1, 0, 0, 0, 0
-A = A.tocsc()
+    # since the area does not necessarily have a rectangular shape, we should set the ones outside this region to identity.
+    for y in range(1, n - 1):
+        for x in range(1, m - 1):
+            if mask[y, x] == 0:  # so if it was in the black area, it means that we should set the row be identity.
+                z = x + y * m  # meaning only the pixel in center is 1 and others are zero.
+                A[z, z], A[z, z + m], A[z, z - m], A[z, z + 1], A[z, z - 1] = 1, 0, 0, 0, 0
+    A = A.tocsc()
+    # apply this on the images
+    src_reshaped, tar_reshaped = img_src.copy().reshape((m * n, 3)), img_tar.copy().reshape((m * n, 3))
+    mask = mask.flatten()
+    b = lap.dot(src_reshaped)
+    b[mask == 0] = tar_reshaped[mask == 0]  # the boundary
+    res = spsolve(A, b).reshape((n, m, 3))  # solve the equation A f = A g
+    res[res > 255], res[res < 0] = 255, 0  # make sure it is in the correct range.
+    cv2.imwrite("../results/res07.jpg", res.astype('uint8'))
+    return res.astype('uint8')
 
-src_reshaped, tar_reshaped = img_src.copy().reshape((m * n, 3)), img_tar.copy().reshape((m * n, 3))
-mask = mask.flatten()
-b = lap.dot(src_reshaped)
-b[mask == 0] = tar_reshaped[mask == 0]
-res = spsolve(A, b).reshape((n, m, 3))
-res[res > 255], res[res < 0] = 255, 0
 
-cv2.imwrite("../results/res07.jpg", res.astype('uint8'))
-
-# mask_flat = mask.flatten()
-# for channel in range(3):
-#     source_flat = img_src[0:n, 0:m, channel].flatten()
-#     target_flat = img_tar[0:n, 0:m, channel].flatten()
-#
-#     mat_b = lap.dot(source_flat)
-#
-#     mat_b[mask_flat == 0] = target_flat[mask_flat == 0]
-#
-#     x = spsolve(A, mat_b)
-#
-#     x = x.reshape((n, m))
-#
-#     x[x > 255] = 255
-#     x[x < 0] = 0
-#     x = x.astype('uint8')
-#
-#     img_tar[0:n, 0:m, channel] = x
+apply_poisson_blending("res05", "res06", "dog_mask", 290, 402)
--- hw5/questions/q3.py
+++ hw5/questions/q3.py
@@ -1,11 +1,26 @@
 import cv2
-import matplotlib.pyplot as plt
-import numpy as np
 
-img_1 = cv2.imread("../images/res08.jpeg").astype(float)
-img_2 = cv2.imread("../images/res09.jpeg").astype(float)
-mask = cv2.imread("../images/cat_bird_mask.jpeg")
-res = np.zeros(img_1.shape)
+
+
+def apply_blending(src_name, tar_name, mask_name):
+    img_1 = cv2.imread("../images/" + src_name + ".jpg").astype(float)
+    img_2 = cv2.imread("../images/" + tar_name + ".jpg").astype(float)
+    mask = cv2.imread("../images/" + mask_name + ".jpg") / 255
+    iter_num, blur_window, window_size_low, window_size_high = 7, 7, 7, 19
+    stack1, stack2 = [], [] # build the stacks for these two images.
+    for iteration in range(iter_num):
+        img_1, lap1 = get_laplacian(blur_window, img_1)
+        img_2, lap2 = get_laplacian(blur_window, img_2)
+        stack1.append(lap1), stack2.append(lap2)
+
+    stack1.append(img_1), stack2.append(img_2)
+    res = apply_feathering(stack1[iter_num], stack2[iter_num], mask, window_size_high)
+
+    for iteration in range(iter_num - 1, -1, -1):
+        res += apply_feathering(stack1[iteration], stack2[iteration], mask, window_size_low)
+
+    cv2.imwrite("../results/res10.jpg", scale(res))
+    return scale(res)
 
 
 # laplacian stack:
@@ -20,37 +35,9 @@ def apply_feathering(src, tar, maskk, k):
     return tar * maskk + src * (1 - maskk)
 
 
-iter_num, blur_window = 7, 7
-stack1, stack2 = [], []
-for iteration in range(iter_num):
-    img_1, lap1 = get_laplacian(blur_window, img_1)
-    img_2, lap2 = get_laplacian(blur_window, img_2)
-    stack1.append(lap1), stack2.append(lap2)
-    plt.imshow(img_1.astype('uint8'))
-    plt.show()
-    plt.imshow(lap1, cmap='gray')
-    plt.show()
-
-stack1.append(img_1), stack2.append(img_2)
-res = apply_feathering(stack1[iter_num], stack2[iter_num], mask, 19)
-
-for iteration in range(iter_num - 1, -1, -1):
-    res += apply_feathering(stack1[iteration], stack2[iteration], mask, 7)
-
-res1 = res.copy()
-res1[res1 > 255] = 255
-# res1[res1 < 0] = 0
-res1 = res1.astype('uint8')
-
-res2 = res.copy()
-
-
 def scale(array):
     min, max = array.min(), array.max()
     return ((array - min) * (1 / (max - min) * 255)).astype('uint8')
 
 
-res2 = scale(res2)
-res2[res2 == 0] = res1[res2 == 0]
-
-cv2.imwrite("../results/res10.jpg", res2)
+apply_blending("res08", "res09", "cat_bird_mask")
